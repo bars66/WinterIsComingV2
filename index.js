95342a59c03d3393a2863f4b8a4dd7a104f97c02
Nikolay Pavlushin <i@bars66.com>
2018-09-26 01:32:13 +0300

    Bot

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const logger = require('logzio-nodejs').createLogger({
   host: 'listener.logz.io',
   type: 'winterIsComing2'
 });
+const TelegramBot = require('node-telegram-bot-api');
+const bot = new TelegramBot(process.env.TG_BOT, {polling: true});
+
+let iObj = {};
 
 const SERIAL_PORT = process.env.SERIAL_PORT;
 
@@ -18,16 +22,20 @@ port.on('data', function (dataBuffer) {
   const [type, data] = dataString.split('=');
 
   if (type === 'I') {
-    const [canaltTmp, insideTmp, timeDelta, heaterPower, ventEnabled] = data.split(';');
+    const [canaltTmp, insideTmp, timeDelta, heaterPower, ventEnabled, heaterEnabled, temp] = data.split(';');
     const frequency = (1 / timeDelta) * 1000000;
     const heaterWatts = heaterPower * 240;
 
-    logger.log({
+    iObj = {
       msgType: type,
       canaltTmp: +canaltTmp, insideTmp: +insideTmp, timeDelta, heaterPower: +heaterPower, ventEnabled: Boolean(ventEnabled),
+      heaterEnabled: Boolean(heaterEnabled),
+      temp: +temp,
       frequency: +frequency / 2, heaterWatts,
-    });
+    };
 
+    logger.log(iObj);
+    console.log(JSON.stringify(iObj, null, 4));
     return;
   }
 
@@ -35,4 +43,53 @@ port.on('data', function (dataBuffer) {
     msgType: type,
     data,
   });
+});
+
+bot.onText(/disabled/, (msg, match) => {
+  port.write('00 24.5');
+  const chatId = msg.chat.id;
+
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, 'DISABLED');
+  setTimeout(() => {
+    bot.sendMessage(chatId, JSON.stringify(iObj, null, 2));
+  }, 10 * 1000);
+});
+
+bot.onText(/enabled/, (msg, match) => {
+  port.write('11 24.5');
+  const chatId = msg.chat.id;
+
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, 'ENABLED');
+  setTimeout(() => {
+    bot.sendMessage(chatId, JSON.stringify(iObj, null, 2));
+  }, 10 * 1000);
+});
+
+bot.onText(/status/, (msg, match) => {
+  const chatId = msg.chat.id;
+
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, JSON.stringify(iObj, null, 2));
+});
+
+bot.onText(/iamadmin (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+
+  const chatId = msg.chat.id;
+  const resp = match[1];
+  port.write(resp);
+
+  // send back the matched "whatever" to the chat
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, `SEND ${resp}`);
+  setTimeout(() => {
+    bot.sendMessage(chatId, JSON.stringify(iObj, null, 2));
+  }, 10 * 1000);});
+
+bot.on('polling_error', (error) => {
+  console.log(error);  // => 'EFATAL'
 });
