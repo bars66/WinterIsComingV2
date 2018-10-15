087ac4ccd1622fb8fa4291b5771dc2823bc2bb1f
Nikolay Pavlushin <i@bars66.com>
2018-10-16 01:00:31 +0300

    Build project

--- build/controllers/telegramController.js
+++ build/controllers/telegramController.js
@@ -9,6 +9,8 @@ var _nodeTelegramBotApi = _interopRequireDefault(require("node-telegram-bot-api"
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; var ownKeys = Object.keys(source); if (typeof Object.getOwnPropertySymbols === 'function') { ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) { return Object.getOwnPropertyDescriptor(source, sym).enumerable; })); } ownKeys.forEach(function (key) { _defineProperty(target, key, source[key]); }); } return target; }
+
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
 class TelegramController {
@@ -27,7 +29,12 @@ class TelegramController {
     _defineProperty(this, "cmd_status", (msg, match) => {
       this.logMessage(msg);
       const chatId = msg.chat.id;
-      this.bot.sendMessage(chatId, JSON.stringify(this.control, null, 2));
+
+      const cnt = _objectSpread({}, this.control, {
+        tgBot: undefined
+      });
+
+      this.bot.sendMessage(chatId, JSON.stringify(cnt, null, 2));
     });
 
     this.control = control;
