393a71e2e30c6222cd4887c39b23dc50407010d9
Nikolay Pavlushin <i@bars66.com>
2018-11-26 02:20:38 +0300

    Build project

--- build/controllers/telegramController.js
+++ build/controllers/telegramController.js
@@ -70,9 +70,10 @@ class Telegram {
 
         if (!run) {
           this.bot.sendMessage(chatId, `function not found`);
+          return;
         }
 
-        const result = await run().bind(this);
+        const result = run().bind(this);
         this.bot.sendMessage(chatId, JSON.stringify(result, null, 2));
       } catch (e) {
         this.bot.sendMessage(chatId, `error`);
