57d2cab3e8769bb9cb1e9ea11b3ff4c34d998ad8
Nikolay Pavlushin <i@bars66.com>
2019-03-08 22:48:42 +0300

    fix

--- src/controllers/ventController.js
+++ src/controllers/ventController.js
@@ -75,8 +75,8 @@ export class Vent {
   setTemp = (temp) => {
     this.checkPort()
     const { ventEnabled, heaterEnabled } = this.params
-    const tempLength = `${+temp}`
-    const sendStr = `${+ventEnabled}${+heaterEnabled} ${temp}${tempLength === 2 ? '.0' : ''}`
+    const tempStr = `${+temp}`
+    const sendStr = `${+ventEnabled}${+heaterEnabled} ${tempStr}${tempStr.length === 2 ? '.0' : ''}`
     this.logger.debug(`SEND::: ${sendStr}`)
     this.port.write(sendStr.trim());
     this.params = {
@@ -101,8 +101,8 @@ export class Vent {
           insideTmp: +insideTmp,
           timeDelta,
           heaterPower: +heaterPower,
-          ventEnabled: Boolean(ventEnabled),
-          heaterEnabled: Boolean(heaterEnabled),
+          ventEnabled: Boolean(+ventEnabled),
+          heaterEnabled: Boolean(+heaterEnabled),
           temp: +temp,
           frequency: +frequency / 2,
           heaterWatts
